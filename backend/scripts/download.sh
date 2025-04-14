c2ace501f7e820715a3a3e6a506e5244093c40ba
Javier Dreves <jdreves@buk.cl>
2025-04-14 10:42:33 -0300

    fix hls test path

--- backend/scripts/download.sh
+++ backend/scripts/download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Descargando segmentos desde Google Drive..."
-cd /app/hls_test
+cd /app
 
 # Intentar descargar con diferentes métodos
 if ! gdown --fuzzy --no-cookies "https://drive.google.com/file/d/1exGq6BJ6r1lXezOanp88sWwxqcMbDntJ/view?usp=sharing"; then
@@ -12,7 +12,7 @@ fi
 if [ -f "hls test.zip" ]; then
     echo "Descomprimiendo archivos..."
     unzip "hls test.zip"
-    rm hls test.zip
+    rm "hls test.zip"
     echo "Segmentos preparados correctamente"
 else
     echo "Error: No se pudo descargar el archivo"
